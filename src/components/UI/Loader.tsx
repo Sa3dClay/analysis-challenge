efe3b69495c07a7e36e1117bca15419ce3d96d84
AbdulRahman <30749261+Sa3dClay@users.noreply.github.com>
2022-09-07 20:06:42 +0200

    install react loader fetch data from firebase

---
+++ src/components/UI/Loader.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { InfinitySpin } from "react-loader-spinner";
+
+export const Loader = () => {
+  return (
+    <div className="grid justify-center content-center h-screen">
+      <InfinitySpin width="200" color="#4fa94d" />
+    </div>
+  );
+};
--- src/pages/Analysis.tsx
+++ src/pages/Analysis.tsx
@@ -1,9 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Loader } from "../components/UI/Loader";
 
 const Analysis = () => {
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const fetchData = async () => {
+    setIsLoading(true);
+    setError(null);
+
+    const response = await fetch(
+      "https://analysis-507f4-default-rtdb.firebaseio.com/data.json"
+    );
+    const json = await response.json();
+
+    if (!response.ok) setError(json.error);
+    else console.log(json);
+
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const analysisTemplate = <h1 className="text-center">Analysis Page</h1>;
+
   return (
     <>
-      <h1 className="text-center">Analysis Page</h1>
+      {isLoading && <Loader />}
+      {!isLoading && error && <p className="text-center">{error}</p>}
+      {!isLoading && !error && analysisTemplate}
     </>
   );
 };
