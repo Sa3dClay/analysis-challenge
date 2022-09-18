ee063d1a7b3af1900e24687235c9c42c0b7011ee
AbdulRahman <30749261+Sa3dClay@users.noreply.github.com>
2022-09-18 11:27:10 +0200

    avoid showing icon text before layout rendered

--- src/components/UI/ThemeToggler.tsx
+++ src/components/UI/ThemeToggler.tsx
@@ -1,11 +1,16 @@
-import React from "react";
+import React, { useLayoutEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { StoreStateType } from "../../store";
 import { uiDataActions } from "../../store/ui.slice";
 
 const ThemeToggler = () => {
   const dispatch = useDispatch();
+  const [iconText, setIconText] = useState("");
   const uiData = useSelector((state: StoreStateType) => state.uiData);
+
+  useLayoutEffect(() => {
+    uiData.darkTheme ? setIconText("toggle_off") : setIconText("toggle_on");
+  }, [uiData.darkTheme]);
 
   const toggleThemeHandler = () => {
     dispatch(uiDataActions.toggleTheme());
@@ -14,16 +19,13 @@ const ThemeToggler = () => {
   return (
     <>
       <button className="fixed top-2 right-4" onClick={toggleThemeHandler}>
-        {!uiData.darkTheme && (
-          <span className="material-symbols-outlined text-4xl text-gray-800">
-            toggle_on
-          </span>
-        )}
-        {uiData.darkTheme && (
-          <span className="material-symbols-outlined text-4xl text-gray-200">
-            toggle_off
-          </span>
-        )}
+        <span
+          className={`material-symbols-outlined text-4xl ${
+            uiData.darkTheme ? "text-gray-200" : "text-gray-800"
+          }`}
+        >
+          {iconText}
+        </span>
       </button>
     </>
   );
